703b83e9fea273a742c70cba422f53ec9b2070fa
Rohitdey2697 <rohit.dey2697@gmail.com>
2024-10-04 15:50:15 +0530

    added more features

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,4 +1,4 @@
 import mongoose from 'mongoose';
 import logger from '../logger';
 import env from '../env';
-mongoose.connect(`mongodb+srv://${encodeURIComponent(env.mongo_username as string)}:${encodeURIComponent(env.mongo_password as string)}@atlascluster.bv99nzt.mongodb.net/`).then(() => logger.info("connected")).catch(err => logger.error(err))
+mongoose.connect(`mongodb+srv://${encodeURIComponent(env.mongo_username as string)}:${encodeURIComponent(env.mongo_password as string)}@atlascluster.bv99nzt.mongodb.net/newsdb`).then(() => logger.info("connected")).catch(err => logger.error(err))
